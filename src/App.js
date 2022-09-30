b29a3fc8d0a900c87f5df057ec3aa284ebf97c2e
dianix0330 <dianix0330@gmail.com>
2022-09-29 19:31:01 -0700

    First release of test-react-concepts

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react'
+import './App.css';
+
+const initialState = {count: 0};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error('Hey not valid action!')
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div className="App">
+      Count: {state.count}      
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+    </div>
+  );
+}
+
+export default App;
